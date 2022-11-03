4e7bb4789024f5fd90bb1ee8d6360efab330cf09
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-03 23:08:10 +0100

    functioning quicksort!

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -2,48 +2,42 @@ use std::cmp::Ord;
 
 pub fn sort<T>(collection: &mut [T]) 
 where 
-    T: Ord + std::fmt::Debug + Copy,
+    T: Ord + Copy,
 {
-    if collection.is_empty() {
+    if collection.len() <= 1 {
         return;
     }
 
     // Use last element as the pivot element.
-    let mut pivot_index = collection.len() - 1;
-    let pivot = collection[pivot_index];
+    let pivot = collection[collection.len() - 1];
 
     let mut cursor = 0;
     let mut less_than_index = 0;
     let mut larger_than_index = collection.len() - 1;
 
     loop {
-        let test = &collection[cursor];
-        println!("Next element: {:?}", test);
         if collection[cursor] < pivot {
-            println!("Less than! This needs to go to the front.");
-            if cursor <= less_than_index {
-                // If the element is already in the start, it's great. Do nothing.
-            } else {
+            if cursor > less_than_index {
                 collection.swap(cursor, less_than_index + 1);
                 less_than_index += 1;
             }
             cursor += 1;
-
         } else if collection[cursor] == pivot {
-            println!("Equal to pivot! Moving on..");
             cursor += 1;
         } else if collection[cursor] > pivot {
-            // If the element is larger than the pivot, move it to the back and
-            // don't advance the cursor.
-            println!("Larger! Swapping to the back..");
+            // If the element is larger, swap it with the next from the back.
+            // In this case, we do not want to increment the cursor!
             collection.swap(cursor, larger_than_index);
             larger_than_index -= 1;
         }
-        println!("{:?}", collection);
         if cursor >= larger_than_index {
             break;
         }
     }
+
+    let (less_than, larger_than) = collection.split_at_mut(less_than_index + 1);
+    sort(less_than);
+    sort(larger_than);
 }
 
 #[cfg(test)]
@@ -51,8 +45,9 @@ mod tests {
     use crate::sort;
 
     #[test]
-    fn it_works() {
-        let mut vec = vec![1, 4, 2, 9, 5, 2, 1, 8, 3, 3, 6, 6, 7, 3, 5];
+    fn random() {
+        let mut vec = vec![1, 4, 2, 9, 5, 2, 1, 8, 3, 3, 6, 6, 7, 3, 2];
         sort(&mut vec);
+        println!("{:?}", vec);
     }
 }
