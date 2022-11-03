ad4fb890155c9ab850bb3d7ee2aeb81f4f75063a
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-03 21:17:22 +0100

    completes one iteration (using copy)

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -2,36 +2,47 @@ use std::cmp::Ord;
 
 pub fn sort<T>(collection: &mut [T]) 
 where 
-    T: Ord + std::fmt::Debug,
+    T: Ord + std::fmt::Debug + Copy,
 {
     if collection.is_empty() {
         return;
     }
 
     // Use last element as the pivot element.
-    let pivot_index = collection.len() - 1;
+    let mut pivot_index = collection.len() - 1;
+    let pivot = collection[pivot_index];
 
-    let mut front = 0;
-    let mut back = collection.len() - 1;
+    let mut cursor = 0;
+    let mut less_than_index = 0;
+    let mut larger_than_index = collection.len() - 1;
 
     loop {
-        while collection[front] < collection[pivot_index] {
-            front += 1;
-        }
+        let test = &collection[cursor];
+        println!("Next element: {:?}", test);
+        if collection[cursor] < pivot {
+            println!("Less than! This needs to go to the front.");
+            if cursor <= less_than_index {
+                // If the element is already in the start, it's great. Do nothing.
+            } else {
+                collection.swap(cursor, less_than_index + 1);
+                less_than_index += 1;
+            }
+            cursor += 1;
 
-        while collection[back] > collection[pivot_index] {
-            back -= 1;
+        } else if collection[cursor] == pivot {
+            println!("Equal to pivot! Moving on..");
+            cursor += 1;
+        } else if collection[cursor] > pivot {
+            // If the element is larger than the pivot, move it to the back and
+            // don't advance the cursor.
+            println!("Larger! Swapping to the back..");
+            collection.swap(cursor, larger_than_index);
+            larger_than_index -= 1;
         }
-
-        if front >= back {
-            break;
-        } else {
-            collection.swap(front, back);
-            front += 1;
-            back -= 1;
-        }
-        println!("Front: {front}, Back: {back}");
         println!("{:?}", collection);
+        if cursor >= larger_than_index {
+            break;
+        }
     }
 }
 
@@ -41,7 +52,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut vec = vec![1, 4, 6, 7, 3, 3, 5, 6, 5];
+        let mut vec = vec![1, 4, 2, 9, 5, 2, 1, 8, 3, 3, 6, 6, 7, 3, 5];
         sort(&mut vec);
     }
 }
