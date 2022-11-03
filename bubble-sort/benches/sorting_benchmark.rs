014aef9203e3663f92739a5f7e93cd732c9b49f0
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-03 23:50:53 +0100

    lower sample size

--- bubble-sort/benches/sorting_benchmark.rs
+++ bubble-sort/benches/sorting_benchmark.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
-    let mut vec = (0..50000).map(|_| rng.gen_range(0, 1000)).collect::<Vec<_>>();
+    let mut vec = (0..5000).map(|_| rng.gen_range(0, 1000)).collect::<Vec<_>>();
     c.bench_function("bubbles-sort", |b| b.iter(|| sort(black_box(&mut vec))));
 }
 
