0324381f4879ae2d6549a716d7e57d897fe81edc
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-04 15:21:11 +0100

    first try on merge sort

---
+++ merge_sort/src/merge_sort.rs
@@ -0,0 +1,44 @@
+pub fn sort<T>(collection: &mut [T])
+    where T: Ord + std::fmt::Debug
+{
+    if collection.len() > 1 {
+        // println!("Splitting {:?} into two parts", collection);
+        let (front, back) = collection.split_at_mut(collection.len() / 2);
+        sort(front);
+        sort(back);
+    } else {
+        // A single value is considered sorted.
+        return;
+    }
+
+    let mut front_index = 0;
+    let mut back_index = 0;
+
+    let mut sorted: Vec<&T> = Vec::with_capacity(collection.len());
+
+    println!("Pre sorted: {:?}", collection);
+    // Merge front and back into sorted.
+    while front_index < collection.len() / 2 && back_index < collection.len() / 2 {
+        if collection[front_index] < collection[collection.len() / 2 + back_index] {
+            sorted.push(&collection[front_index]);
+            front_index += 1;
+        } else {
+            sorted.push(&collection[collection.len() / 2 + back_index]);
+            back_index += 1;
+        }
+    }
+    println!("Post sorted: {:?}", sorted);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut collection = vec![5, 4, 1, 3, 2];
+        sort(&mut collection);
+        assert_eq!(collection, vec![1, 2, 3, 4, 5]);
+    }
+}
+
